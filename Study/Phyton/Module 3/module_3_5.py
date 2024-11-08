c3a5db2b53a4dd7dc98fd9512dfd16bff174516c
vladoss33 <vladossos42@gmail.com>
2024-11-08 16:53:49 +0300

    Add files via upload

---
+++ Study/Phyton/Module 3/module_3_5.py
@@ -0,0 +1,10 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    str_number= str_number.replace('0', '')
+    first= int(str_number[0])
+    if len(str_number) > 1:
+        return first*get_multiplied_digits(int(str_number[1:]))
+    else:
+        return first
+result = get_multiplied_digits(40203)
+print(result)
